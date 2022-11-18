501fa8d888de7bf2b34a34e4e3c86aaa5b5091d8
Fikrii2210 <muhammadalfikri50@gmail.com>
2022-11-18 17:53:33 +0700

    membuat struktur folder dan template

--- Unity-School-Frontend/webpack.dev.js
+++ Unity-School-Frontend/webpack.dev.js
@@ -1,10 +1,20 @@
-const {merge} = require("webpack-merge");
-const path = require("path");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-    mode: "development",
-    devServer: {
-        port: 9000
-    }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 9011,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    compress: true,
+  },
 });
