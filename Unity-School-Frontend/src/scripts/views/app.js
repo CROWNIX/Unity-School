eab988fd4a47e49581cbf74f58504963e781f1cb
CROWNIX <rahmatfauzi841@gmail.com>
2022-11-28 14:00:33 +0700

    Memperbaiki landing page

--- Unity-School-Frontend/src/scripts/views/app.js
+++ Unity-School-Frontend/src/scripts/views/app.js
@@ -3,13 +3,11 @@ import web from '../routes/web';
 
 class App {
   constructor(content) {
-    console.log(content);
     this._content = content;
   }
 
   async renderPage() {
     const url = UrlParser.parseActiveUrlWithCombiner();
-    console.log(url);
     const page = web[url];
     this._content.innerHTML = await page.render();
     await page.afterRender();
---
+++ Unity-School-Frontend/src/scripts/views/pages/admin/dashboard.js
@@ -0,0 +1,11 @@
+const dashboard = {
+  async render() {
+    return `
+            <h1>Dashboard</h1>
+        `;
+  },
+
+  async afterRender() {},
+};
+
+export default dashboard;
